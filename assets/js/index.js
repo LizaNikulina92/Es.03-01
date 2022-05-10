386f0aeb154d8818cddfc9c1f7ccde10e7056492
LizaNikulina92 <fleurdelys_f@hotmail.it>
2022-05-10 16:58:05 +0200

    js calcolatore

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ var cognome = window.prompt('Cognome');
 var eta = window.prompt('Età');
 var citta = window.prompt('Città');
 var email = window.prompt('Email');
+var fullname = nome+' '+cognome+' '+eta+' '+citta+' '+'('+email+')'
 
 document.getElementById('nome_js').innerHTML=nome;
 document.getElementById('cognome_js').innerHTML=cognome;
@@ -10,6 +11,8 @@ document.getElementById('eta_js').innerHTML=eta;
 document.getElementById('citta_js').innerHTML=citta;
 document.getElementById('email_js').innerHTML=email;
 
-document.getElementById('recap_js').innerHTML=nome+' '+cognome+' '+eta+' '+citta+' '+'('+email+')';
+document.getElementById('recap_js').innerHTML=fullname;
 
-console.log(nome+' '+cognome+' '+eta+' '+citta+' '+'('+email+')')
+console.log(fullname)
+document.write(fullname)
+window.alert(fullname)
